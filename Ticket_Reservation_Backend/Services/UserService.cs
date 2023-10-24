b460923afaa4cf6cbbf1231b59e736128c9cd40c
kamindu.g <kamindu.g@informaticsint.com>
2023-10-24 13:55:55 +0530

    deactivate accout

--- Ticket_Reservation_Backend/Services/UserService.cs
+++ Ticket_Reservation_Backend/Services/UserService.cs
@@ -18,60 +18,74 @@ using BCrypt.Net;
 
 namespace TicketReservation.Services;
 
-public class UserService {
+public class UserService
+{
     private readonly IMongoCollection<User> _users;
 
-    public UserService(IOptions<MongoDBSettings> mongoDBSettings) {
-       MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionString);
-    IMongoDatabase database = client.GetDatabase("TrainGoDB");
-    _users = database.GetCollection<User>("Users");
+    public UserService(IOptions<MongoDBSettings> mongoDBSettings)
+    {
+        MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionString);
+        IMongoDatabase database = client.GetDatabase("TrainGoDB");
+        _users = database.GetCollection<User>("Users");
     }
 
 
     //create a new user(traveler) in the TrainGoDB
-    public async Task CreateAsync(User user) {
+    public async Task CreateAsync(User user)
+    {
         var filter = Builders<User>.Filter.Eq("Nic", user.Nic);
         var user1 = await _users.Find(filter).FirstOrDefaultAsync();
-        if (user1 != null) {
+        if (user1 != null)
+        {
             Console.WriteLine("User already exists");
             return;
-        }else{
-           string hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
-                user.Password = hashedPassword;
-                await _users.InsertOneAsync(user);
-                Console.WriteLine("User created");
         }
-        
+        else
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            user.Password = hashedPassword;
+            await _users.InsertOneAsync(user);
+            Console.WriteLine("User created");
+        }
+
         return;
     }
 
 
     //login user from checking NIC and password
-    public async Task<User> loginAsync(string nic  , string password) {
+
+    public async Task<User> loginAsync(string nic, string password)
+    {
         Console.WriteLine("User login");
         var filter = Builders<User>.Filter.Eq("Nic", nic);
-            var user = await _users.Find(filter).FirstOrDefaultAsync();
+        var user = await _users.Find(filter).FirstOrDefaultAsync();
+        if (user != null && BCrypt.Net.BCrypt.Verify(password, user.Password))
+        {
+            if (user.Active == false)
+            {
+                throw new Exception("Account Deactivated");
+            }
+            return user;
+        }
+        else
+        {
+            throw new Exception("Invalid Credentials");
+        }
 
-            if (user != null && BCrypt.Net.BCrypt.Verify(password, user.Password))
-            {
-                return user;
-            }
-            else
-            {
-              throw new Exception("Invalid Credentials");
-            }
     }
 
 
     //get all users(travelers) list from the TrainGoDB
-    public async Task<List<User>> GetUsersAsync() {
+    public async Task<List<User>> GetUsersAsync()
+    {
         Console.WriteLine("User get");
         return await _users.Find(new BsonDocument()).ToListAsync();
 
     }
 
     //get details of a specific user by the user ID
-    public async Task<User> GetUserByIdAsync(string id) {
+    public async Task<User> GetUserByIdAsync(string id)
+    {
         Console.WriteLine($"Getting user by ID: {id}");
         var filter = Builders<User>.Filter.Eq("Id", id);
         return await _users.Find(filter).FirstOrDefaultAsync();
@@ -79,7 +93,8 @@ public class UserService {
 
 
     //update user details by the user ID
-    public async Task UpdateUserAsync(string id, User user) {
+    public async Task UpdateUserAsync(string id, User user)
+    {
         FilterDefinition<User> filter = Builders<User>.Filter.Eq("Id", id);
         UpdateDefinition<User> update = Builders<User>.Update.Set("Username", user.Username).Set("Password", user.Password).Set("Email", user.Email).Set("Nic", user.Nic).Set("Active", user.Active);
         await _users.UpdateOneAsync(filter, update);
@@ -87,27 +102,28 @@ public class UserService {
     }
 
     //delete a specific user form the db by the user ID
-    public async Task DeleteAsync(string id) {
+    public async Task DeleteAsync(string id)
+    {
         FilterDefinition<User> filter = Builders<User>.Filter.Eq("Id", id);
         await _users.DeleteOneAsync(filter);
         return;
     }
 
     //activate a specific user account
-    public async Task DeactivateAsync(string id) {
+    public async Task DeactivateAsync(string id)
+    {
         FilterDefinition<User> filter = Builders<User>.Filter.Eq("Id", id);
         UpdateDefinition<User> update = Builders<User>.Update.Set("Active", false);
         await _users.UpdateOneAsync(filter, update);
         return;
     }
-    
+
     //deactivate a specific user account 
-    public async Task ActivateAsync(string id){
+    public async Task ActivateAsync(string id)
+    {
         FilterDefinition<User> filter = Builders<User>.Filter.Eq("Id", id);
         UpdateDefinition<User> update = Builders<User>.Update.Set("Active", true);
         await _users.UpdateOneAsync(filter, update);
         return;
     }
-    
-    
 }
